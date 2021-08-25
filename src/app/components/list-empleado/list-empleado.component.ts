edbc59651a8e458ffef324a49c41a6a2303e3981
David <gmunoz@seedem.co>
2021-08-25 10:55:01 -0500

    Iterando empleado list

--- src/app/components/list-empleado/list-empleado.component.ts
+++ src/app/components/list-empleado/list-empleado.component.ts
@@ -2,26 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { EmpleadoService } from 'src/app/services/empleado.service';
+import { Empleado } from 'src/app/models/empleado';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-list-empleado',
@@ -29,22 +12,31 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./list-empleado.component.scss']
 })
 export class ListEmpleadoComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA) ; 
+  displayedColumns: string[] = ['nombreCompleto', 'telefono', 'correo', 'fechaIngreso' , 'estadoCivil' , 'sexo' , 'acciones' ];
+  dataSource = new MatTableDataSource() ; 
+  listEmpleado: Empleado[] ; 
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator , { static:true }) paginator: MatPaginator ;
 
-  constructor() { }
+  constructor( private empleadoService: EmpleadoService ) { }
 
   ngOnInit(): void {
+    this.cargarEmpleados() ; 
     this.dataSource.paginator = this.paginator ; 
     this.dataSource.sort = this.sort;
+    
   }
 
   apliFilter(event : Event){
     const filterValue = (event.target as HTMLInputElement).value ; 
     this.dataSource.filter = filterValue.trim().toLocaleLowerCase(); 
+  }
+
+  cargarEmpleados(){
+    this.listEmpleado = this.empleadoService.getEmpleados();
+    this.dataSource = new MatTableDataSource(this.listEmpleado) ; 
+    console.log(this.listEmpleado) ; 
   }
 
 }
---
+++ src/app/services/empleado.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Empleado } from '../models/empleado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+  listEmpleado: Empleado[] = [
+    { nombreCompleto: 'camilo Aristizabal' , telefono: '8754444' , correo: 'juan@gmail.com' , fechaIngreso: new Date() , estadoCivil: 'soltero' , sexo: 'Masculino' } , 
+    { nombreCompleto: 'Mariluz suarez' , telefono: '8754444' , correo: 'juan@gmail.com' , fechaIngreso: new Date() , estadoCivil: 'soltero' , sexo: 'Masculino' } ,
+    { nombreCompleto: 'Laura vanesa ' , telefono: '8754444' , correo: 'juan@gmail.com' , fechaIngreso: new Date() , estadoCivil: 'soltero' , sexo: 'Masculino' } ,
+    { nombreCompleto: 'Maria camila' , telefono: '8754444' , correo: 'juan@gmail.com' , fechaIngreso: new Date() , estadoCivil: 'soltero' , sexo: 'Masculino' } ,
+    { nombreCompleto: 'Pedro arango ' , telefono: '8754444' , correo: 'juan@gmail.com' , fechaIngreso: new Date() , estadoCivil: 'soltero' , sexo: 'Masculino' } 
+  ] ; 
+
+
+
+  constructor() { }
+
+  getEmpleados(){
+    return this.listEmpleado.slice(); 
+  }
+}
