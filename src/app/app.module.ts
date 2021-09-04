2085c0d148f929755fcc055d5af32b942071723d
David <machov12@gmail.com>
2021-09-04 12:01:39 -0500

    Agregar empleado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -26,7 +25,7 @@ import { ReactiveFormsModule } from "@angular/forms";
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialnModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Empleado } from 'src/app/models/empleado';
+import { EmpleadoService } from 'src/app/services/empleado.service';
 
 @Component({
   selector: 'app-add-edit-empleado',
@@ -15,7 +19,14 @@ export class AddEditEmpleadoComponent implements OnInit {
   estadosCiviles:any[] = ['soltero', 'casado' , 'divorciado' ];
   myForm: FormGroup
 
-  constructor(private fb: FormBuilder) { 
+  constructor(
+    private fb: FormBuilder,
+    private empleadoService: EmpleadoService,
+    private route: Router,
+    private snacBar: MatSnackBar
+
+  ) { 
+    
     this.myForm = this.fb.group({
       nombreCompleto: [''],
       correo: [''],
@@ -29,4 +40,19 @@ export class AddEditEmpleadoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  guardarEmpleado(){
+    const empleado: Empleado = {
+      nombreCompleto:this.myForm.get('nombreCompleto').value, 
+      correo:this.myForm.get('correo').value, 
+      fechaIngreso:this.myForm.get('fechaIngreso').value, 
+      telefono:this.myForm.get('telefono').value, 
+      estadoCivil:this.myForm.get('estadoCivil').value, 
+      sexo:this.myForm.get('sexo').value, 
+    } ; 
+    this.empleadoService.agregarEmpleado(empleado) ; 
+    this.snacBar.open('El empleado fue registrado exitosamente', '' , {
+      duration: 3000
+    }) ; 
+    this.route.navigate(['/']);
+  }
 }
